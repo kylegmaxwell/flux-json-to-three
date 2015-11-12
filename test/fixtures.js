38500e3214a9a42a9a9b26e923e04b9c6e9c8839
DnMllr <me@dnmllr.io>
2015-11-12 14:10:18 -0800

    added tests
    Change-Id: I0dd4a28601fe519ee12885c6f1ffcacd1d20baad

---
+++ test/fixtures.js
@@ -0,0 +1,281 @@
+/*
+ * A set of fixtures for the parasolid util tests
+ */
+
+export var cone = {
+    "input": {
+        "direction":[1,1,1],
+        "height":10,
+        "origin":[0,0,0],
+        "primitive":"cone",
+        "radius":10,
+        "semi-angle":10
+    },
+    "result": {
+        "type": "Mesh"
+    }
+};
+
+export var cylinder = {
+    "input": {
+        "axis":[10,20,30],
+        "height":40,
+        "origin":[0,0,0],
+        "primitive":"cylinder",
+        "radius":10
+    },
+    "result": {
+        "type": "Mesh"
+    }
+};
+
+export var sphere = {
+    "input": {
+        "origin":[0,0,0],
+        "primitive":"sphere",
+        "radius":10
+    },
+    "result": {
+        "type": "Mesh"
+    }
+};
+
+export var torus = {
+    "input": {
+        "origin": [0,0,0],
+        "majorRadius": 5,
+        "minorRadius":3,
+        "axis":[0,0,1],
+        "primitive":"torus"
+    },
+    "result": {
+        "type": "Mesh"
+    }
+};
+
+export var block = {
+    "input": {
+        "origin":[0,0,0],
+        "dimensions":[1,2,3],
+        "axis":[0,0,1],
+        "reference":[0,1,0],
+        "primitive":"block"
+    },
+    "result": {
+        "type": "Mesh"
+    }
+};
+
+
+export var circle = {
+     "input": {
+         "origin":[0,0,0],
+         "primitive":"circle",
+         "radius":10
+     },
+     "result": {
+         "type": "Mesh"
+     }
+};
+
+export var rectangle = {
+    "input": {
+        "dimensions": [2,2],
+        "primitive": "rectangle"
+    },
+    "result": {
+        "type": "Mesh"
+    }
+};
+
+export var plane = {
+    "input": {
+        "origin":[5,5,0],
+        "normal":[0,0,1],
+        "primitive":"plane"
+    },
+    "result": {
+        "type": "Mesh"
+    }
+};
+
+export var point = {
+    "input": {
+        "point":[0,0,0],
+        "primitive":"point"
+    },
+    "result": {
+        "type":"Points"
+    }
+};
+
+export var vector = {
+    "input": {
+        "coords": [2,2,0], 
+        "primitive": "vector"
+    },
+    "result": {
+        "type": "Object3D"
+    }
+};
+
+export var line = {
+    "input": {
+        "end":[2,2,2],
+        "primitive":"line",
+        "start":[1,1,1]
+    },
+    "result": {
+        "type": "Line"
+    }
+};
+
+export var curve = {
+    "input": {
+        "controlPoints":[[0,0,0],[1,0,0],[1,1,0],[0,1,0]],
+        "degree":3,
+        "knots":[0,0,0,1,2,3,3,3],
+        "primitive":"curve"
+    },
+    "result": {
+        "type": "Line"
+    }
+};
+
+export var polycurve = {
+    "input": {
+        "curves":[{
+            "controlPoints":[[0,0,0],[1,0,0],[1,1,0],[0,1,0]],
+            "degree":3,"knots":[0,0,0,1,2,3,3,3],
+            "primitive":"curve"
+        }, {
+            "degree":3,
+            "knots":[0,0,0,0,14.1,14.1,14.1,14.1],
+            "controlPoints":[[0,0,0],[-3.3,-3.3,0],[-6.6,-6.6,0],[-10,-10,0]],
+            "primitive":"curve"
+        }],
+        "primitive":"polycurve"
+    },
+    "result": {
+        "type": "Object3D"
+    }
+};
+
+export var arc = {
+    "input": {
+        "start":[1,0,0],
+        "middle":[0,1,0],
+        "end":[-1,0,0],
+        "primitive":"arc"
+    },
+    "result": {
+        "type": "Line"
+    }
+};
+
+export var degenerateArc = { // test degenerate arc
+    "input": {
+        "start":[0,0,0],
+        "middle":[1,1,0],
+        "end":[0,0,0],
+        "primitive":"arc"
+    },
+    "result": {
+        "type": "Line"
+    }
+};
+
+export var mesh = {
+    "input": {
+         "vertices": [[-1,0,0],[0,1,2],[1,0,0],[0,-1,2]],
+         "faces":[[0,3,1],[1,3,2]],
+         "primitive":"mesh"
+    },
+    "result": {
+        "type": "Mesh"
+    }
+};
+
+export var polygonSet = {
+    "input":{
+        "polygons":[{
+            "boundary":[[15,0,0],[-7.5,13.0,0],[-7.5,-13.0,0]],
+            "holes":[]
+        }],
+        "primitive":"polygon-set"
+    },
+    "result": {
+        "type": "Mesh"
+    },
+};
+
+export var polyline = {
+    "input": {
+        "points":[[0,0,5],[1,0,5],[2,2,5],[0,1,5]],
+        "primitive":"polyline"
+    },
+    "result": {
+        "type": "Line"
+    }
+};
+
+export var surface = {
+    "input": {
+        "controlPoints":[[[-8,8,0],[8,8,0]],[[-8,-8,0],[8,-8,0]]],
+        "primitive":"surface",
+        "uDegree":1,
+        "uKnots":[0,0,1,1],
+        "vDegree":1,
+        "vKnots":[0,0,1,1]
+    },
+    "result": {
+        "type": "Mesh"
+    }
+};
+
+export var polysurface = {
+    "input": {
+        "surfaces":[{
+            "controlPoints":[[[-8,8,0],[8,8,0]],[[-8,-8,0],[8,-8,0]]],
+            "primitive":"surface",
+            "uDegree":1,
+            "uKnots":[0,0,1,1],
+            "vDegree":1,
+            "vKnots":[0,0,1,1]
+        }, {
+            "controlPoints":[[[-20,8,9],[-8,8,0]],[[-20,-8,9],[-8,-8,0]]],
+            "primitive":"surface",
+            "uDegree":1,
+            "uKnots":[0,0,1,1],
+            "vDegree":1,
+            "vKnots":[0,0,1,1]
+        }],
+        "primitive":"polysurface"
+    },
+    "result": {
+        "type": "Object3D"
+    }
+};
+
+export var text = {
+    "input": {
+        "align":[0,0,0],
+        "direction":[0,-1,100],
+        "origin":[0,-7,1],
+        "primitive":"text",
+        "size":8,
+        "text":"Text!"
+    },
+    "result": {
+        "type": "textHelper"
+    }
+};
+
+export var invalid = {
+    "input": {
+        "info": "Not supported", 
+        "primitive": "brep"
+    },
+    "result": null
+};
+
---
+++ test/test.js
@@ -0,0 +1,31 @@
+
+
+'use strict'
+
+import test from 'tape';
+import createObject from '../index.js'
+import * as fixtures from './fixtures.js';
+
+test( 'flux-parasolidUtil', function ( t ) {
+    var obj = createObject( 3 ),
+        key,
+        input;
+
+    t.equal( obj.mesh, null, 'it should ignore non geometric types' );
+
+    for ( key in fixtures ) {
+        input = fixtures[ key ].input;
+        obj = createObject( input );
+
+        if ( input.result ) {
+            t.ok( obj.mesh, 'createObject should create a mesh for parasolid data of ' + key  );
+            t.equal( obj.mesh.type, fixtures[ key ].result.type, 'createObject should create a mesh of type ' +
+                                                                 fixtures[ key ].result.type + ' for data ' + key ); 
+        } else {
+            t.ok( obj.invalidPrims[ input.primitive ], 'if the data is invalid, createObject' +
+                                                       ' should return it as part of' +
+                                                       ' the set of invalid primitives' ); 
+        }
+    }
+
+});
