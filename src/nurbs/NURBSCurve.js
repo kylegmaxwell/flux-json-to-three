e07763556e8fc4d7284bd274e4934b69136aed05
Kyle Maxwell <kyle@flux.io>
2017-06-20 17:11:01 -0600

    Fix for nurbs curves - PLT-6777
    Some NURBS curves were being evaluated at the wrong parameters
    causing them to be too long and extrapolate, now we always
    limit them, regardless of whether they are closed.
    
    :viewport:
    
    Code Review:
    1. Check out related CLs.
    2. FORCE_RELOAD_STDLIB=1 make run-viewport
    QA:
    1. Download Curve_JS.json and render in flux viewport.
    2. It should look like screenshot-1.png not CurveFlow.JPG
    
    Change-Id: I9bbb02717783e2fb34ba9b8012a47dfc9a9f73fa

--- src/nurbs/NURBSCurve.js
+++ src/nurbs/NURBSCurve.js
@@ -29,31 +29,13 @@ export default function NURBSCurve ( degree, knots, controlPoints ) {
 		var point = controlPoints[i];
 		this.controlPoints[i] = new THREE.Vector4(point.x, point.y, point.z, point.w);
 	}
-	// Closed curves force their first span to be a single point. Since a
-	// span consists of a number of knots equal to the degree, we must skip
-	// the first knots to avoid extrapolation artifacts for parametric values
-	// outside the domain of the curve. iMin and iMax are integer Numbers that
-	// store the beginning and ending index which contain the renderable
-	// domain of the curve.
-	this.iMin = 0;
-	this.iMax = this.knots.length-1;
-	if (this.isClosed()) {
-		this.iMin = this.degree;
-		this.iMax = this.knots.length-1-this.degree;
-	}
+	// For some reason Flux does not use the full parameter range from 0 to knots.length-1
+	this.iMin = this.degree;
+	this.iMax = this.knots.length-1-this.degree;
 }
-
 
 NURBSCurve.prototype = Object.create( THREE.Curve.prototype );
 NURBSCurve.prototype.constructor = NURBSCurve;
-
-NURBSCurve.prototype.isClosed = function ( ) {
-	var start = this._getPointByParameter(this.knots[this.degree]);
-	var end = this._getPointByParameter(this.knots[this.knots.length-1-this.degree]);
-	start.sub(end);
-	var TOLERANCE = 0.000001;
-	return start.length() < TOLERANCE;
-};
 
 NURBSCurve.prototype.getPoint = function ( t ) {
 	var u = this.knots[this.iMin] + t * (this.knots[this.iMax] - this.knots[this.iMin]); // linear mapping t->u
@@ -82,4 +64,3 @@ NURBSCurve.prototype.getTangent = function ( t ) {
 
 	return tangent;
 };
-
